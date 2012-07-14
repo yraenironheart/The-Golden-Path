9d616fdadcc8c1d8dbe30598b979ed46d65b8d2c
Yraen Ironheart <yraen.ironheart@gmail.com>
2012-07-14 23:15:36 +1000

    Added Probe

---
+++ modules/Probe.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Author: Yraen Ironheart
+ * User: Sarif
+ * Date: 24/06/12
+ * Time: 2:46 PM
+ *
+ * Variable dumper
+ */
+class Probe {
+
+	/**
+	 * Output using print_r
+	 *
+	 * @param $data
+	 */
+	static public function dump($data) {
+		echo "<PRE>";
+		print_r($data);
+		echo "</PRE>";
+	}
+}
