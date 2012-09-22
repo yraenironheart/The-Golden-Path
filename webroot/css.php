d4b5b5bde9e11cca507ef9e209811cdda659811c
Yraen Ironheart <yraen.ironheart@gmail.com>
2012-09-23 04:06:19 +1000

    Add Core_Agglutinator_Css and Core_Agglutinator_Js files

---
+++ webroot/css.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Author: Yraen Ironheart
+ * Date: 8/08/12
+ * Time: 9:56 PM
+ *
+ * Agglutinate css/js file contents into one big chunk to reduce the number
+ * of requests per page
+ */
+require_once('../modules/Core/Bootstrap.php');
+
+$agglutinator = new Core_Agglutinator_Css($_GET['files']);
+$agglutinator->stream();
--- webroot/js.php
+++ webroot/js.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * Author: Yraen Ironheart
- * Date: 5/08/12
- * Time: 12:40 AM
+ * Date: 8/08/12
+ * Time: 9:56 PM
  *
  * Agglutinate css/js file contents into one big chunk to reduce the number
  * of requests per page
  */
 require_once('../modules/Core/Bootstrap.php');
 
-$agglutinator = new Agglutinator($_GET['files']);
+$agglutinator = new Core_Agglutinator_Js($_GET['files']);
 $agglutinator->stream();
